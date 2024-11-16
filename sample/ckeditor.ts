ca2aa1287cce0b16fbf4a716061a334a228d553d
Konstantin Kozokar <konstantin@neotalogic.com>
2024-11-16 18:14:00 +0100

    Init commit

--- sample/ckeditor.ts
+++ sample/ckeditor.ts
@@ -26,87 +26,93 @@ import {
 	MediaEmbed,
 	Paragraph,
 	Table,
-	TableToolbar
+	TableToolbar,
 } from 'ckeditor5';
 
 import CKEditorInspector from '@ckeditor/ckeditor5-inspector';
 
-import LlmConnector from '../src/llmconnector.js';
+import { GhostText } from '@thesis/ckeditor5-ghost-text';
 
 import 'ckeditor5/ckeditor5.css';
+import '@thesis/ckeditor5-ghost-text/index.css';
 
-ClassicEditor
-	.create( document.getElementById( 'editor' )!, {
-		plugins: [
-			LlmConnector,
-			Essentials,
-			Autoformat,
-			BlockQuote,
-			Bold,
-			Heading,
-			Image,
-			ImageCaption,
-			ImageStyle,
-			ImageToolbar,
-			ImageUpload,
-			Indent,
-			Italic,
-			Link,
-			List,
-			MediaEmbed,
-			Paragraph,
-			Table,
-			TableToolbar,
-			CodeBlock,
-			Code,
-			Base64UploadAdapter
-		],
+ClassicEditor.create(document.getElementById('editor')!, {
+	plugins: [
+		GhostText,
+		Essentials,
+		Autoformat,
+		BlockQuote,
+		Bold,
+		Heading,
+		Image,
+		ImageCaption,
+		ImageStyle,
+		ImageToolbar,
+		ImageUpload,
+		Indent,
+		Italic,
+		Link,
+		List,
+		MediaEmbed,
+		Paragraph,
+		Table,
+		TableToolbar,
+		CodeBlock,
+		Code,
+		Base64UploadAdapter,
+	],
+	toolbar: [
+		'undo',
+		'redo',
+		'|',
+		'ghostTextButton',
+		'|',
+		'heading',
+		'|',
+		'bold',
+		'italic',
+		'link',
+		'code',
+		'bulletedList',
+		'numberedList',
+		'|',
+		'outdent',
+		'indent',
+		'|',
+		'uploadImage',
+		'blockQuote',
+		'insertTable',
+		'mediaEmbed',
+		'codeBlock',
+	],
+	image: {
 		toolbar: [
-			'undo',
-			'redo',
+			'imageStyle:inline',
+			'imageStyle:block',
+			'imageStyle:side',
 			'|',
-			'llmConnectorButton',
-			'|',
-			'heading',
-			'|',
-			'bold',
-			'italic',
-			'link',
-			'code',
-			'bulletedList',
-			'numberedList',
-			'|',
-			'outdent',
-			'indent',
-			'|',
-			'uploadImage',
-			'blockQuote',
-			'insertTable',
-			'mediaEmbed',
-			'codeBlock'
+			'imageTextAlternative',
 		],
-		image: {
-			toolbar: [
-				'imageStyle:inline',
-				'imageStyle:block',
-				'imageStyle:side',
-				'|',
-				'imageTextAlternative'
-			]
+	},
+	table: {
+		contentToolbar: ['tableColumn', 'tableRow', 'mergeTableCells'],
+	},
+	ghostText: {
+		debounceDelay: 1000,
+		contentFetcher: async () => {
+			return new Promise((resolve) =>
+				setTimeout(() => {
+					resolve('I need a dollar');
+				}, 2000)
+			);
 		},
-		table: {
-			contentToolbar: [
-				'tableColumn',
-				'tableRow',
-				'mergeTableCells'
-			]
-		}
-	} )
-	.then( editor => {
+	},
+})
+	.then((editor) => {
 		window.editor = editor;
-		CKEditorInspector.attach( editor );
-		window.console.log( 'CKEditor 5 is ready.', editor );
-	} )
-	.catch( err => {
-		window.console.error( err.stack );
-	} );
+		CKEditorInspector.attach(editor);
+		window.console.log('CKEditor 5 is ready.', editor);
+	})
+	.catch((err) => {
+		window.console.error(err.stack);
+	});
