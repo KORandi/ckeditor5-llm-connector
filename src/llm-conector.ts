1554a3f8b3645f447f770f1cf52fd4d793492479
Konstantin Kozokar <konstantin@neotalogic.com>
2024-11-20 17:52:59 +0100

    Update import

--- src/llm-conector.ts
+++ src/llm-conector.ts
@@ -1,8 +1,9 @@
 import { Dialog, Plugin } from 'ckeditor5';
+import LlmConnectorParameterConfigUI from './llm-connector-parameter-config-ui';
 
 export default class LlmConnector extends Plugin {
 	static get requires() {
-		return [Dialog];
+		return [Dialog, LlmConnectorParameterConfigUI];
 	}
 
 	public static get pluginName(): 'LlmConnector' {
